06cedeaf8b0610add5419a36a371c15c5bac9b38
Bogdan Chadkin <trysound@yandex.ru>
2015-11-12 14:23:53 +0300

    Initial

---
+++ README.md
@@ -0,0 +1,45 @@
+# rollup-plugin-memory
+
+Load entry from memory
+
+## Install
+
+```sh
+npm i rollup-plugin-memory -D
+```
+
+## Usage
+
+```js
+import { rollup } from 'rollup';
+import memory from 'rollup-plugin-memory';
+
+rollup({
+    entry: 'main.js',
+    plugins: [
+        memory([
+            contents: 'console.log("some code");'
+        ])
+    ]
+});
+```
+
+## Options
+
+### options.contents
+
+Required. Type: `string` or `Buffer`
+
+Preloaded content
+
+### options.path
+
+Options. Type: `string`
+
+Path to loaded content. If this options specified rollup `entry` will be skipped
+
+> rollup `entry` still should contain any non-empty string
+
+# License
+
+MIT © [Bogdan Chadkin](mailto:trysound@yandex.ru)
---
+++ src/index.js
@@ -0,0 +1,33 @@
+function once(fn) {
+    var called = false;
+    return (...args) => {
+        if (!called) {
+            called = true;
+            return fn(...args);
+        }
+    };
+}
+
+export default function memory(opts = {}) {
+    if (typeof opts.contents !== 'string' &&
+        !(opts.contents instanceof Buffer)
+    ) {
+        throw Error([
+            'rollup-plugin-memory',
+            'opts.contents should be string or buffer instance'
+        ].join(': '));
+    }
+
+    let contents = opts.contents.toString();
+    let path = typeof opts.path === 'string' ? opts.path : false;
+
+    return {
+        resolveId: once(id => {
+            return path || id;
+        }),
+
+        load: once(() => {
+            return contents;
+        })
+    };
+}
